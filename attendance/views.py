5355bde4f895395c5f455e617301fb92833c432b
Damien Abeloos <msyukyu.dev@gmail.com>
2023-04-08 22:27:24 +0200

    [IMP] attendance: integrate owl framework

--- attendance/views.py
+++ attendance/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+# views
+def index(request):
+    """View function for home page of site."""
+
+    context = {
+        'hello_context': "Hello Context!",
+    }
+
+    return render(request, 'index.html', context=context)
